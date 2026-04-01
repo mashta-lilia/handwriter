68a47df2b19970a1d58dbf54a8002855de1bbe4e
Lilia Mashta <mashta.lilia@gmail.com>
2026-04-01 10:05:22 +0300

    Add tg_chat_id field to User model

--- backend/models/user.py
+++ backend/models/user.py
@@ -1,13 +1,15 @@
 from datetime import datetime
-from sqlalchemy import String, Boolean, DateTime, func
+from sqlalchemy import String, Boolean, DateTime, BigInteger, func
 from sqlalchemy.orm import Mapped, mapped_column
 from core.database import Base
+
 
 class User(Base):
     __tablename__ = "users"
 
     id: Mapped[int] = mapped_column(primary_key=True)
     tg_username: Mapped[str] = mapped_column(String(64), unique=True, index=True, nullable=False)
+    tg_chat_id: Mapped[int | None] = mapped_column(BigInteger, unique=True, nullable=True)
     password_hash: Mapped[str] = mapped_column(String(256), nullable=False)
     is_active: Mapped[bool] = mapped_column(Boolean, default=False)
     created_at: Mapped[datetime] = mapped_column(
